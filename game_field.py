e0b4a14b14cda63f43ee2c82bd5d023932b18c6b
zannabianca1997 <zannabianca199712@gmail.com>
2017-06-01 18:00:05 +0200

    While downloading IDE written a basic tile and video setup

---
+++ game_field.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  1 17:00:46 2017
+
+@author: zanna
+"""
+from enum import Enum, auto
+
---
+++ labirint.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  1 17:00:46 2017
+
+@author: zanna
+"""
+import json
+
+SETUPFILE = "setup.json"
+
+
+if __name__ == "__main__":
+    with open(SETUPFILE) as setup_file:
+        setups = json.load(setup_file)
