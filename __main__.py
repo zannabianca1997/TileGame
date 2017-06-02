a6725649b65c64c8828938d09491859e6826cdc0
zannabianca1997 <zannabianca199712@gmail.com>
2017-06-02 11:50:30 +0200

    Added logging class and loggers

--- __main__.py
+++ __main__.py
@@ -5,12 +5,18 @@ Created on Thu Jun  1 17:00:46 2017
 @author: zanna
 """
 import json
+import logging
 import game_field
 
+# logging setup
+logger = logging.getLogger(__name__)
+
+#setup file
 SETUPFILE = "setup.json"
 
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
     with open(SETUPFILE) as setup_file:
         setups = json.load(setup_file)
         game_field.init(setups["game"])
--- game_field.py
+++ game_field.py
@@ -5,7 +5,11 @@ Created on Thu Jun  1 17:00:46 2017
 @author: zanna
 """
 import numpy as np
+import logging
 import tiled
+
+# logging setup
+logger = logging.getLogger(__name__)
 
 walls_types = None
 floors_types = None
